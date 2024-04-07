17293e2a25cdc2483b01f9941bf599923cdae360
MilaDi2023 <133892507+MilaDi2023@users.noreply.github.com>
2024-04-07 09:08:55 +0300

    Update home_task.py

--- home_task.py
+++ home_task.py
@@ -16,26 +16,36 @@ class User:
         self.__id = id
         self.__name = name
         self.__access_level = "user"
+
     def get_id(self):
         return self.__id
+
     def get_name(self):
         return self.__name
+
     def get_access_level(self):
         return self.__access_level
+
     def user_info(self):
         return f"Это приватные сведения о сотруднике: ID - {self.__id}, ФИО - {self.__name}, уровень доступа - {self.__access_level}"
+
 all_users = []
+
 class Admin(User):
     def __init__(self, id, name):
         super().__init__(id, name)
         self._User__access_level = "admin"
+
     def add_user(self, user):
         all_users.append(user)
+
     def remove_user(self, id):
         for user in all_users:
             if user.get_id() == id:
                 all_users.remove(user)
+
 employee1 = User("007", "James Bond")
 print(employee1.user_info())
+
 employee2 = Admin("001", "Super Mario")
 print(employee2.user_info())
